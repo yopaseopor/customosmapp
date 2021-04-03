53e689576f767195fe0e9933a0fe6b3e426ec738
yopaseopor <yopaseopor@gmail.com>
2021-04-03 09:23:11 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -847,17 +847,27 @@ var config = {
 		  group: 'Test',
 		  title: 'tracks2',
 		  geojson: 'https://raw.githubusercontent.com/yopaseopor/osmhistoricmap/master/src/tracks.geojson',
-		  style: new ol.style.Style({
-							stroke: new ol.style.Stroke({
-								color: 'rgba(255, 255, 255, 1.0)',
-								width: 6
-							}),
-							text: new ol.style.Text({
-								text: '1'
-								placement: 'line'
-							})
-          }))
-      })
+			iconSrc: imgSrc + 'base/circle.svg',
+			iconStyle: 'background-color:#0000ff',
+			style: function () {
+				var fill = new ol.style.Fill({
+					color: 'rgba(0,0,255,0.4)'
+				});
+				var stroke = new ol.style.Stroke({
+					color: '#0000ff',
+					width: 1.25
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Circle({
+						fill: fill,
+						stroke: stroke,
+						radius: 5
+					}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
 		},
 	{
       group: 'Test',
