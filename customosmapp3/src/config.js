9b9583ab7fc8691192af7bbba0fa43f1873e1b19
yopaseopor <yopaseopor@gmail.com>
2021-12-01 23:07:23 +0100

    Update config.js

--- customosmapp3/src/config.js
+++ customosmapp3/src/config.js
@@ -42,7 +42,6 @@ var config = {
 			source: new ol.source.OSM()
 		}),
 		new ol.layer.Tile({
-			group: 'Test2',
 			title: 'OpenStreetMap B&W',
 			iconSrc: imgSrc + 'osmbw_logo-layer.png',
 			source: new ol.source.XYZ({
@@ -62,7 +61,6 @@ var config = {
 			visible: false
 		}),
 		new ol.layer.Tile({
-			group: 'Test2',
 			title: 'OpenMapSurfer',
 			iconSrc: imgSrc + 'openroute_logo_layer.png',
 			source: new ol.source.XYZ({
@@ -1231,7 +1229,6 @@ var config = {
 			group: 'Test',
 			title: 'Pere IV',
 			iconSrc: 'https://raw.githubusercontent.com/yopaseopor/beta_preset_josm/master/ES/traffic_signs/ES/ES_R2.png',
-						attributions: '&copy; <a href="https://www.openstreetmap.org/" target="_blank">OpenStreetMap2</a>',
 			geojson: 'https://raw.githubusercontent.com/yopaseopor/customosmapp/master/customosmapp3/src/img/base/2020_bcn_Carrer_de_Pere_IV.geojson',
 			style: function (feature) {
 				var name = feature.get('name') || '';
